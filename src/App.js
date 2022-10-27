18669b70345c95214a62c110dfc72d0ff46e238e
kasongoj <Joseph.Kasongo@sckans.edu>
2022-10-27 12:05:03 -0500

    edit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+
+import FAQ from './components/FAQ';
 
 import logo from './logo.svg';
 import './App.css';
@@ -7,12 +9,21 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      Home
+      
 
     <BrowserRouter>
-      <Route>
-        <Route path="/infinite-site/account" element={<div> > Account</div>} />
-      </Route>
+
+      <div className='nav'>
+      <Link to="/infinite-site">Home</Link>
+      <Link to="/infinite-site/account">Account</Link>
+      <Link to="/infinite-site/faq">FAQ</Link>
+      </div>
+
+      <Routes>
+      <Route path="/infinite-site" element={<div>Home</div>} />
+        <Route path="/infinite-site/account" element={<div>Account</div>} />
+        <Route path="/infinite-site/faq" element={<FAQ />} />?
+      </Routes>
     </BrowserRouter>
       
     </div>
---
+++ src/components/FAQ.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const FAQ = () => {
+    return (
+        <div>
+            Frequently Asked Questions
+            <ul>
+                <li>
+                    What is React?
+                </li>
+                <li>
+                    How does the react Router work?
+                </li>
+            </ul>
+        </div>
+    )
+}
+
+export default FAQ;
